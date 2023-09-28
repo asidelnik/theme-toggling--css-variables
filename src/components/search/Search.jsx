df0c6b1c8ee75c7a605571bb05df018b5a3e60eb
Amos <asidelnik@gmail.com>
2023-09-28 17:20:01 +0300

    Search - placeholder & label - fix

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,24 +1,20 @@
 import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Search() {
   return (
     <>
       <Box sx={{ '& > :not(style)': { m: 1 } }}>
         <FormControl variant="standard">
-          <InputLabel htmlFor="input-with-icon-adornment">
-            With a start adornment
-          </InputLabel>
-
           <Input
             id="input-with-icon-adornment"
+            placeholder="Search episodes"
             startAdornment={
               <InputAdornment position="start">
-                <AccountCircle />
+                <SearchIcon />
               </InputAdornment>
             }
           />
