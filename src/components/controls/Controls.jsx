38cdd3540c847d4d57747d8f286eac4eaac9e779
Amos <asidelnik@gmail.com>
2023-09-28 19:11:34 +0300

    Progress bar & controls

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -1,3 +1,36 @@
+import './controls.css';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import Replay30Icon from '@mui/icons-material/Replay30';
+import Forward30Icon from '@mui/icons-material/Forward30';
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import Progress from '../linear-progress/LinearProgress';
+
 export default function Controls() {
-  return <></>;
+  return (
+    <>
+      <section className="controls">
+        <div className="bookmark">
+          <BookmarkIcon />
+        </div>
+        <div className="rewind">
+          <Replay30Icon />
+        </div>
+        <div className="pause-play">
+          <PlayCircleFilledIcon />
+          <PauseCircleIcon />
+        </div>
+        <div className="forwards">
+          <Forward30Icon />
+        </div>
+        <div className="speed">1.0x</div>
+
+        <div className="from-time">00:00</div>
+        <div className="progress-bar">
+          <Progress />
+        </div>
+        <div className="to-time">00:00</div>
+      </section>
+    </>
+  );
 }
