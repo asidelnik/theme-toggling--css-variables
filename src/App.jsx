ea9bec44727fd9394a3b30a00d5301fdc4c97923
Amos <asidelnik@gmail.com>
2023-09-28 15:27:32 +0300

    Theme - complete successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  // const [theme, setTheme] = useState('light-theme');
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+      document.body.className = currentTheme;
+    } else {
+      const x = prefersDarkScheme.matches ? 'dark-theme' : 'light-theme';
+      localStorage.setItem('theme', x);
+      document.body.className = x;
+    }
+  }, []);
+
+  function toggleTheme() {
+    const isDarkTheme = document.body.classList.contains('dark-theme');
+    const newTheme = isDarkTheme ? 'light-theme' : 'dark-theme';
+
+    localStorage.setItem('theme', newTheme);
+    document.body.className = newTheme;
+  }
 
   return (
     <>
@@ -12,6 +33,7 @@ function App() {
         <a
           href="https://vitejs.dev"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <img
             src={viteLogo}
@@ -22,6 +44,7 @@ function App() {
         <a
           href="https://react.dev"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <img
             src={reactLogo}
@@ -32,18 +55,23 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={toggleTheme}>Toggle theme</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
 
-export default App;
+// Get the user's theme preference from local storage, if it's available
+const currentTheme = localStorage.getItem('theme');
+// If the user's preference in localStorage is dark...
+if (currentTheme == 'dark') {
+  // ...let's toggle the .dark-theme class on the body
+  document.body.classList.toggle('dark-mode');
+  // Otherwise, if the user's preference in localStorage is light...
+} else if (currentTheme == 'light') {
+  // ...let's toggle the .light-theme class on the body
+  document.body.classList.toggle('light-mode');
+}
