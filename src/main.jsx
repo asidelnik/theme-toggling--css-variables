e6b4c8a49fc3193d351a011decb1ccac12090e3e
Amos <asidelnik@gmail.com>
2023-09-29 12:17:38 +0300

    lint

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import './styling/root.css';
 import './styling/theme.css';
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
